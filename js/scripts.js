0a437d535551a70a4a723878f8123789d1fc9aac
Nomdeplum Vovan <tuzenkov50@gmail.com>
2018-12-20 22:15:18 +0200

    Update 2018-12-20T20:15:16.405Z

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
 
 
-function close(){
+window.onload
+
+function openMenu() {
+
   var element = document.getElementById('closeMenu');
-  element.classList.toggle('closed');
-  console.log("element.classList");
+  element.classList.add('nav__mobile');
 }
+
+function closeMenu() {
+  
+  var element = document.getElementById('closeMenu');
+  element.classList.remove('nav__mobile');
+}
+
+function toggleMenu(){
+   
+  var element = document.getElementById('closeMenu');
+  element.classList.toggle('nav__mobile');
+}
+
+// document.querySelector('closed').addEventListener('click', classToggle);
+// document.querySelector('open').addEventListener('click', classToggle);
 
